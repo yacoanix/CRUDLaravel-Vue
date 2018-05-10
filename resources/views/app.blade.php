9003be735139a1a0fd9fdfd2b14be82e04853102
yacoanix <aymedeyacoan@gmail.com>
2018-05-10 11:44:28 +0100

    creada migracion de task, seed, controlador, rutas api y etc

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,22 +12,7 @@
     </head>
     <body>
         <div id="main" class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h1>VUEjs - AJAX axios</h1>
-                    <ul class="list-group">
-                        <li v-for="item in lists" class="list-group-item">
-                            @{{ item.name }}
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-8">
-                    <h1>JSON</h1>
-                    <pre>
-                        @{{ $data }}
-                    </pre>
-                </div>
-            </div>
+            @yield('content')
         </div>
         <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+Route::resource('tasks', 'TaskController', ['except' => 'show']);
+
